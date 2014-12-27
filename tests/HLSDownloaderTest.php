f846cc87f67f6e8cb0efe82836fd4df6c6528846
Ejz <ejz@ya.ru>
2014-12-28 01:04:09 +0300

    Update HLSDownloaderTest.php

--- tests/HLSDownloaderTest.php
+++ tests/HLSDownloaderTest.php
@@ -7,10 +7,16 @@
 use Ejz\HLSDownloader;
 
 class HLSDownloaderTest extends PHPUnit_Framework_TestCase {
-    public function testSimple() {
-        set_error_handler(function($_, $__) {
-            echo $__, chr(10);
-        });
-        HLSDownloader::go("http://ejz.ru/hls/hls-flat/trailer.m3u8");
+    public function testFlat() {
+        $path = HLSDownloader::go("http://ejz.ru/hls/hls-flat/trailer.m3u8");
+        $this -> assertEquals(21, count(scandir($path)));
+    }
+    public function testLevelUp() {
+        $path = HLSDownloader::go("http://ejz.ru/hls/hls-2/trailer.m3u8");
+        $this -> assertEquals(21, count(scandir($path)));
+    }
+    public function testLevelDown() {
+        $path = HLSDownloader::go("http://ejz.ru/hls/hls-3/trailer.m3u8");
+        $this -> assertEquals(21, count(scandir($path)));
     }
 }
