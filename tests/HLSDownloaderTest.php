698549c6400af5f486875660dde15cc19492d001
Ejz <ejz@ya.ru>
2014-12-28 00:51:12 +0300

    Update HLSDownloaderTest.php

--- tests/HLSDownloaderTest.php
+++ tests/HLSDownloaderTest.php
@@ -8,5 +8,9 @@ use Ejz\HLSDownloader;
 
 class HLSDownloaderTest extends PHPUnit_Framework_TestCase {
     public function testSimple() {
+        set_error_handler(function($_, $__) {
+            echo $__, chr(10);
+        });
+        HLSDownloader::go("http://ejz.ru/hls/hls-flat/trailer.m3u8");
     }
 }
