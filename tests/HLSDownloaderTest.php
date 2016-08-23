95ddc3fe13f0ed596a309cac2679ffb26dc2d1fa
Ejz <ejz@ya.ru>
2016-08-23 22:26:41 +0300

    _

--- tests/HLSDownloaderTest.php
+++ tests/HLSDownloaderTest.php
@@ -7,19 +7,19 @@
 use Ejz\HLSDownload;
 
 class HLSDownloadTest extends PHPUnit_Framework_TestCase {
-    /*
-    public function testEjzLinks() { return;
-        foreach (array("http://site.com/hls/hls-flat/trailer.m3u8", "http://site.com/hls/hls-2/trailer.m3u8", "http://site.com/hls/hls-3/trailer.m3u8") as $link) {
-            $tmp = rtrim(`mktemp -d`);
-            $result = HLSDownload::go($link, ['dir' => $tmp]);
-            $this->assertTrue($result);
-            $this->assertTrue(count(glob($tmp . '/*.m3u8')) === 1);
-            $this->assertTrue(count(glob($tmp . '/stream*')) > 1);
-            $this->assertTrue(count(glob($tmp . '/stream0/*.m3u8')) === 1);
-            $this->assertTrue(count(glob($tmp . '/stream0/*.ts')) > 1);
-        }
+    public function testCommon() {
+        $url = getenv("URL") ?: "";
+        if (!$url) return;
+        $tmp = rtrim(`mktemp -d`);
+        $result = HLSDownload::go($url, ['dir' => $tmp]);
+        $this->assertTrue($result);
+        $this->assertTrue(count(glob($tmp . '/*.m3u8')) === 1);
+        $this->assertTrue(count(glob($tmp . '/stream*')) > 1);
     }
-    public function testFilter() { return;
+    public function testFilter() {
+        $url = getenv("URL") ?: "";
+        if (!$url) return;
+        return;
         $link = "http://site.com/hls/hls-flat/trailer.m3u8";
         //
         $tmp = rtrim(`mktemp -d`);
