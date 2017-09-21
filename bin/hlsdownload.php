fb036c4197fc6f8276e6ee4f6128b37ca3025fab
Ejz <ejz@ya.ru>
2017-09-21 12:22:40 +0300

    _

--- bin/hlsdownload.php
+++ bin/hlsdownload.php
@@ -14,6 +14,7 @@ if (version_compare('5.6.0', PHP_VERSION, '>'))
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
     _err("Windows OS is not supported!");
 
+$argv = isset($_SERVER['argv']) ? $_SERVER['argv'] : array();
 $opts = getopts([
     'h' => false, 'help' => 'h',
     'd' => true, 'directory' => 'd',
@@ -21,26 +22,53 @@ $opts = getopts([
     'no-ts' => false,
     'no-decrypt' => false,
     'limit-rate' => true,
-]);
+    'url' => true,
+], $argv);
 
+if (!isset($opts[0])) _err("Internal error!");
 if (isset($opts['h'])) goto help;
+if (isset($opts['url'])) {
+    $opts[1] = $opts['url'];
+    unset($opts[1]);
+}
 if (!isset($opts[1]) or isset($opts[2])) goto help;
 
 $settings = array();
-if (isset($opts['d'])) $settings['dir'] = $opts['d'];
+$settings['dir'] = isset($opts['d']) ? $opts['d'] : '.';
 if (isset($opts['F'])) $settings['filters'] = $opts['F'];
 if (isset($opts['limit-rate'])) $settings['limit_rate'] = $opts['limit-rate'];
 $settings['decrypt'] = !isset($opts['no-decrypt']);
 $settings['no-ts'] = isset($opts['no-ts']);
-$res = HLSDownload::go($opts[1], $settings);
-exit($res ? 0 : 1);
+if ($settings['no-ts']) {
+    $res = HLSDownload::go($opts[1], $settings);
+    exit($res ? 0 : 1);
+}
+$dir = $settings['dir'];
+$res = HLSDownload::go($opts[1], ['no-ts' => true] + $settings);
+foreach (scandir($dir) as $elem) {
+    if ($elem == '..' or $elem == '.') continue;
+    if (strpos($elem, 'stream') === false) continue;
+    if (!is_dir($dir . '/' . $elem)) continue;
+    $argv_ = $argv;
+    $argv_[] = "-d";
+    $argv_[] = $dir . '/' . $elem;
+    $argv_[] = '--url';
+    $argv_[] = $dir . '/' . $elem . '/' . $elem . '.m3u8';
+    $exec = array_map('escapeshellarg', $argv_);
+    $exec = implode(' ', $exec);
+    $exec = $exec . ' &';
+    echo $exec . "\n";
+    // passthru("{ sleep 1; echo 1; } &");
+    passthru($exec, $err);
+}
+exit(0);
 
 help:
 
 ob_start();
 $ver = VERSION;
 $aut = AUTHOR;
-echo "HLSDownload {$ver} by {$aut}.
+echo "HLSDownload {$ver} by {$aut}
 
 Usage: hlsdownload [options] <M3U8>
 
