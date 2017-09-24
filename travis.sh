d419f97d8cce7a1b57f9c44a87df9e1a1dbc22ff
Ejz <ejz@ya.ru>
2017-09-24 23:54:12 +0300

    _

--- travis.sh
+++ travis.sh
@@ -9,7 +9,9 @@ SQL_HOST=no ./deploy.sh --defaults --test
 ./deploy.sh -D -e ./phar.sh
 set +e
 if ./gitci.sh --is-release; then
-	next=`./gitci.sh --next-tag`
-	cp build/hlsdownload.phar build/hlsdownload-"$next".phar
-	./gitci.sh "build/hlsdownload.phar build/hlsdownload-{$next}.phar" "Release [skip ci]"
+    next=`./gitci.sh --next-tag`
+    cp build/hlsdownload.phar build/hlsdownload-"$next".phar
+    ./gitci.sh "build/hlsdownload.phar build/hlsdownload-{$next}.phar" "Release [skip ci]"
+else
+    ./gitci.sh
 fi
